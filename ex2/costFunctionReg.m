6c2d6c287b75d9d7352afc028c85764294723447
meng <pkucyndi@gmail.com>
2017-03-19 00:24:23 +0900

    ex2 done.

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -6,6 +6,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length(theta);
 
 % You need to return the following variables correctly 
 J = 0;
@@ -18,7 +19,22 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+costsum = 0;
 
+for i = 1:m
+  costsum = costsum + (y(i)*log(sigmoid(X(i,:) * theta)) + (1 - y(i)) * log(1 - sigmoid(X(i,:) * theta)));
+  % h(x(i)) = sigmoid(X(i,:) * theta)
+end
+
+J = -1 / m * costsum + lambda / (2 * m) * ([0 ones(1, n - 1)] * theta.^2);
+
+
+
+grad(1) = 1 / m * X(:,1)' * (sigmoid(X * theta) - y);
+for j = 2:n
+  grad(j) = 1 / m * X(:,j)' * (sigmoid(X * theta) - y) + lambda / m * theta(j);
+  % partial derivative term :  1 / m * ( X' * (h(X) - y))
+end
 
 
 
